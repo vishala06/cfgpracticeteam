eca51c420c00af4dc1f5a02f089790fe601bcc20
Vishala Pakide <vishalapakide@gmail.com>
2026-09-17 14:48:05 +0530

    Add initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Backend is working!"
+    });
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
