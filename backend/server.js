5ec6b7b66d31d4a9cd797766e7a35c99bf990d65
Vishala Pakide <vishalapakide@gmail.com>
2026-09-17 15:45:32 +0530

    Connect authentication routes

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const authRoutes = require("./routes/authroutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
     res.json({
         message: "Backend is working!"
     });
-});
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
 });
