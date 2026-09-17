572bac8028ed6c076d12ae7476736da7705b6b84
Sriram <srirambanda0@gmail.com>
2026-09-17 15:46:15 +0530

    connect complaint routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const complaintRoutes = require("./routes/complaintroutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/complaints", complaintRoutes);
 
 app.get("/", (req, res) => {
     res.json({
